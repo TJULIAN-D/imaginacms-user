6fbb3f54b1771c24b327d6ccd333db6fbe7e0a35
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:17:18 -0500

    +add | modules seeds by jobs

--- Database/Seeders/UserDatabaseSeeder.php
+++ Database/Seeders/UserDatabaseSeeder.php
@@ -4,18 +4,21 @@ namespace Modules\User\Database\Seeders;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class UserDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-        $this->call(SentinelGroupSeedTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\User\Database\Seeders",
+      "seeds" => ["SentinelGroupSeedTableSeeder"]
+    ]);
+  }
 }
