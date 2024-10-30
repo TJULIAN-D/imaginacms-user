8096ee6358a9939abeaef811d9e8df0edc9462bb
michael <msolano2505@gmail.com>
2024-10-29 19:32:01 -0500

    updated email of resetPassword

--- Events/Handlers/SendResetCodeEmail.php
+++ Events/Handlers/SendResetCodeEmail.php
@@ -8,18 +8,30 @@ use Modules\User\Events\UserHasBegunResetProcess;
 
 class SendResetCodeEmail
 {
-    /**
-     * @var Mailer
-     */
-    private $mailer;
+  /**
+   * @var Mailer
+   */
+  private $mailer;
 
-    public function __construct(Mailer $mailer)
-    {
-        $this->mailer = $mailer;
-    }
+  public function __construct(Mailer $mailer)
+  {
+    $this->mailer = $mailer;
+    $this->notification = app("Modules\Notification\Services\Inotification");
+  }
 
-    public function handle(UserHasBegunResetProcess $event)
-    {
-        $this->mailer->to($event->user->email)->send(new ResetPasswordEmail($event->user, $event->code));
-    }
+  public function handle(UserHasBegunResetProcess $event)
+  {
+    //$this->mailer->to($event->user->email)->send(new ResetPasswordEmail($event->user, $event->code));
+    $this->notification->to([
+      'email' => $event->user->email
+    ])->push(
+      [
+        'title' => trans('user::messages.reset password'),
+        'view' => 'user::emails.reminder',
+        'setting' => ['saveInDatabase' => 1],
+        'user' => $event->user,
+        'code' => $event->code,
+      ]
+    );
+  }
 }
